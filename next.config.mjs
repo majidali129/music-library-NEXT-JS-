b3fb4dc6828d1091c5e01552c76ff3de8d7d3cb6
Majid Ali <lahmajidali@gmail.com>
2024-07-28 09:36:09 +0500

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // domains: ["images.unsplash.com", "res.cloudinary.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: ""
+      }
+    ]
+  }
+};
 
 export default nextConfig;
